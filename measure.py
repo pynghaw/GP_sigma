d17f7cd76bc1a8c716b0b0bff0ff4eaa5bb43b4a
Chen Pyng Haw <pynghaw5@gmail.com>
2025-06-08 15:48:21 +0800

    update load and store data process

--- measure.py
+++ measure.py
@@ -1,15 +1,18 @@
 import csv
 import time
 from datetime import datetime
+from ui.GP_ui import Ui_Form as Ui_MainForm
+from PySide6.QtWidgets import QWidget, QTableWidgetItem
 
-def run_measure(stop_flag, input_csv='hioki_simulated_data.csv', interval=0.5):
+def run_measure(stop_flag, main_window, input_csv='hioki_simulated_data.csv', interval=0.5):
     try:
         with open(input_csv, mode='r') as infile:
             reader = csv.DictReader(infile)
-            print("Simulating Hioki measurement fetch.")
-
+            print("Simulating Hioki measurement fetch.")          
+            
+                                    
             for row in reader:
-                if stop_flag():  # externally signaled to stop
+                if stop_flag():  
                     print("Measurement stopped by signal.")
                     break
 
@@ -18,9 +21,17 @@ def run_measure(stop_flag, input_csv='hioki_simulated_data.csv', interval=0.5):
                     ocv = float(row['OCV'])
                     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                     print(f"[{timestamp}] Measurement: OCV={ocv}, RI={ri}")
+                    
+                    main_window.add_row_to_table(ocv, ri, timestamp) # add data to table
+                    
                     time.sleep(interval)
+                    
                 except ValueError:
-                    print("Invalid data row, skipping:", row)
+                    print("Invalid data row, skipping:", row)     
+                    
+            main_window.calculate_metrics() # calcualte metrics               
+            
 
     except Exception as e:
         print(f"Error: {e}")
+
--- windows/main_window.py
+++ windows/main_window.py
@@ -4,16 +4,18 @@ from db_connection import get_connection
 from ui.GP_ui import Ui_Form as Ui_MainForm
 from windows.query_window import QueryWindow
 import math
-from decimal import Decimal
 from measure import run_measure
 
+ocv_values = []
+
 class SimulateThread(QThread):
-    def __init__(self):
+    def __init__(self, main_window):
         super().__init__()
         self._stop_requested = False
+        self.main_window = main_window
 
     def run(self):
-        run_measure(self.should_stop)
+        run_measure(self.should_stop, self.main_window)
 
     def stop(self):
         self._stop_requested = True
@@ -26,20 +28,20 @@ class MainWindow(QWidget):
         super().__init__()
         self.ui = Ui_MainForm()
         self.ui.setupUi(self)
-        self.load_data_from_db()
 
         self.setWindowTitle("GP Sigma")
         
         self.ui.lineEdit_6.setText("COM 1")
         self.ui.lineEdit_13.setText("9600")
         # lineEdit_ocv_spec
-        self.ui.lineEdit_14.setText("100")         
-
+        self.ui.lineEdit_14.setText("0")        
+     
         self.ui.pushButton_6.clicked.connect(self.open_query_window) # Search   
         
-        # start and stop meaursing
+        # start, stop and redo(clear)
         self.ui.pushButton.clicked.connect(self.start_measure)
         self.ui.pushButton_2.clicked.connect(self.stop_measure)  
+        self.ui.pushButton_5.clicked.connect(self.redo_measure)  #clear
         
         self.query_window = None 
         self.sim_thread = None
@@ -57,7 +59,7 @@ class MainWindow(QWidget):
         if self.sim_thread and self.sim_thread.isRunning():
             print("Measurement already running.")
             return
-        self.sim_thread = SimulateThread()
+        self.sim_thread = SimulateThread(main_window=self)
         self.sim_thread.start()
         
     # stop 
@@ -66,55 +68,74 @@ class MainWindow(QWidget):
             self.sim_thread.stop()
             self.sim_thread.wait()
             print("Measurement stopped.")
+            
+    # re-do / Clear
+    def redo_measure(self):
+        ocv_values.clear()
 
-        
-    # RI OCV table
-    def load_data_from_db(self):
+        self.ui.tableWidget.setRowCount(0)
+
+        self.ui.lcdNumber.display("0")
+        self.ui.lcdNumber_2.display("0")
+
+        self.ui.lineEdit_14.clear()
+        self.ui.lineEdit_7.clear()    
+        self.ui.lineEdit_8.clear()    
+        self.ui.lineEdit_9.clear()    
+        self.ui.lineEdit_10.clear()  
+        self.ui.lineEdit_11.clear()  
+        self.ui.lineEdit_12.clear()   
+
+            
+    # RI OCV table        
+    def add_row_to_table(self, ocv, ri, timestamp):
         try:
             conn = get_connection()
             cursor = conn.cursor()
-            cursor.execute("SELECT id, ri, ocv FROM hioki_measurements ORDER BY id ASC")
-            rows = cursor.fetchall()
-
-            self.ui.tableWidget.setRowCount(len(rows))
-            self.ui.tableWidget.setColumnCount(3)            
             
-            ocv_values = []
-
-            for row_index, (id, ri, ocv) in enumerate(rows):
-                self.ui.tableWidget.setItem(row_index, 0, QTableWidgetItem(str(id)))
-                self.ui.tableWidget.setItem(row_index, 1, QTableWidgetItem(str(ri)))
-                self.ui.tableWidget.setItem(row_index, 2, QTableWidgetItem(str(ocv)))
-                self.ui.lineEdit_14.setText(f"{row_index + 1}")
-                ocv_values.append(ocv)
-                               
-            mean_spec_sigma = Decimal("1.5") # mean under dashboard
+            cursor.execute(
+                "INSERT INTO hioki_measurements (ocv, ri, timestamp) VALUES (%s, %s, %s)",
+                (ocv, ri, timestamp)
+            )
             
-            # Calculate OCV for average, max, min, standard deviation, deviation and Sigma
-            if ocv_values:
-                avg_ocv = sum(ocv_values) / len(ocv_values)                
-                max_ocv = max(ocv_values)
-                min_ocv = min(ocv_values)
-                std_dev = math.sqrt(sum((x - avg_ocv) ** 2 for x in ocv_values) / len(ocv_values))
-                deviation = max_ocv - min_ocv          
-                new_ocv = avg_ocv - mean_spec_sigma * Decimal(std_dev)
-
-                print(f"OCV → AVG: {avg_ocv:.4f}, MAX: {max_ocv:.4f}, MIN: {min_ocv:.4f}, STD: {std_dev:.4f}, DEV: {deviation:.4f}")
-                
-                self.ui.lineEdit_9.setText(f"{max_ocv:.4f}")
-                self.ui.lineEdit_8.setText(f"{avg_ocv:.4f}")
-                self.ui.lineEdit_7.setText(f"{min_ocv:.4f}")
-                self.ui.lineEdit_12.setText(f"{std_dev:.4f}")  
-                self.ui.lineEdit_10.setText(f"{deviation:.4f}")                 
-                self.ui.lineEdit_11.setText(f"{new_ocv:.4f}")      
-                
-            # lineEdit Conclusion                
-
+            conn.commit()
+            
+            row_index = self.ui.tableWidget.rowCount()
+            self.ui.tableWidget.insertRow(row_index)
+            self.ui.tableWidget.setItem(row_index, 0, QTableWidgetItem(str(row_index + 1)))     
+            self.ui.tableWidget.setItem(row_index, 1, QTableWidgetItem(f"{ri:.4f}"))           
+            self.ui.tableWidget.setItem(row_index, 2, QTableWidgetItem(f"{ocv:.4f}"))          
+            self.ui.lineEdit_14.setText(str(row_index + 1))  
+            self.ui.lcdNumber.display(f"{ri:.4f}")                                  
+            self.ui.lcdNumber_2.display(f"{ocv:.4f}")  
+            ocv_values.append(ocv)
+            
             cursor.close()
             conn.close()
-
+            
         except Exception as e:
             print(f"Failed to load data from database: {e}")
+        
+    def calculate_metrics(self):
+        mean_spec_sigma = 1.5 # mean under dashboard
+        print(len(ocv_values))
+        
+        if ocv_values:
+            avg_ocv = sum(ocv_values) / len(ocv_values)     
+            max_ocv = max(ocv_values)
+            min_ocv = min(ocv_values)
+            std_dev = math.sqrt(sum((x - avg_ocv) ** 2 for x in ocv_values) / len(ocv_values))
+            deviation = max_ocv - min_ocv          
+            new_ocv = avg_ocv - mean_spec_sigma * std_dev
+            
+            self.ui.lineEdit_9.setText(f"{max_ocv:.4f}")
+            self.ui.lineEdit_8.setText(f"{avg_ocv:.4f}")
+            self.ui.lineEdit_7.setText(f"{min_ocv:.4f}")
+            self.ui.lineEdit_12.setText(f"{std_dev:.4f}")  
+            self.ui.lineEdit_10.setText(f"{deviation:.4f}")                 
+            self.ui.lineEdit_11.setText(f"{new_ocv:.4f}")                                 
+
+        
             
     def closeEvent(self, event):
         if self.sim_thread and self.sim_thread.isRunning():
